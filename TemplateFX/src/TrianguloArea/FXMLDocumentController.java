fad847f652fb03130ec3872e35550707341872c9
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-03-21 11:16:46 +0000

    F02 done

--- TemplateFX/src/TrianguloArea/FXMLDocumentController.java
+++ TemplateFX/src/TrianguloArea/FXMLDocumentController.java
@@ -16,69 +16,95 @@ import javafx.scene.control.TextArea;
  *
  * @author jpc
  */
-
 public class FXMLDocumentController implements Initializable {
-    
+
     private static final DecimalFormat df = new DecimalFormat("0.0000");
-    private static final DecimalFormat df2 = new DecimalFormat("0.0"); 
+    private static final DecimalFormat df2 = new DecimalFormat("0.0");
 
     /**
      * Initializes the controller class.
      */
     @FXML
     TextField txfladoA;
-    
+
     @FXML
     TextField txfladoB;
-    
+
     @FXML
     TextField txfladoC;
-    
+
     @FXML
     TextField txfarea;
-    
+
     @FXML
-    public void actionCalcular(ActionEvent e){ 
-        double a = Double.parseDouble(txfladoA.getText());     
-        double b = Double.parseDouble(txfladoB.getText());   
-        double c = Double.parseDouble(txfladoC.getText().replace(",","."));
-        
-        boolean calcular = true;
-        
-        if(a>b+c){
-            txfladoA.setText("ERRO: a>b+c");
-            txfladoA.requestFocus();
-            calcular = false;
-        }
-        if(b>c+a){
-            txfladoB.setText("ERRO: b>c+a");
-            txfladoB.requestFocus();
-            calcular = false;
-        }
-        if(c>a+b){
-            txfladoC.setText("ERRO: c>a+b");
-            txfladoC.requestFocus();
-            calcular = false;
-        }
-        
-        if(calcular == true){
-            double somaABC = a + b + c;
-            double ABCdivididoPor2 = somaABC / 2;
-            double area = Math.sqrt((ABCdivididoPor2)*(ABCdivididoPor2-a)*(ABCdivididoPor2-b)*(ABCdivididoPor2-c));
-            System.out.println("\n\nA="+a+"\nB="+b+"\nC="+c+"\n\nÁrea="+area);
-            txfarea.setText(String.valueOf(df.format(area)));
-            txfarea.setEditable(false);
-        }
-        else{
+    public void actionCalcular(ActionEvent e) {
+        double a = FXMLDocumentController.isNumeric(txfladoA.getText());
+        double b = FXMLDocumentController.isNumeric(txfladoB.getText());
+        double c = FXMLDocumentController.isNumeric(txfladoC.getText());
+
+        // O user inseriu pelo menos uma string
+        if (a == -1 || b == -1 || c == -1) {
+            if(a == -1){
+                txfladoA.setText("Insira um int!");
+                txfladoA.requestFocus();
+            }
+            if(b == -1){
+                txfladoB.setText("Insira um int!");
+                txfladoB.requestFocus();
+            }
+            if(c == -1){
+                txfladoC.setText("Insira um int!");
+                txfladoC.requestFocus();
+            }
+           
             txfarea.setText("Erro!");
             txfarea.setEditable(false);
-        }       
+            
+        } else { // caso todos os números inseridos sejam inteiros
+            boolean calcular = true;
+            if (a > b + c) {
+                txfladoA.setText("ERRO: a>b+c");
+                txfladoA.requestFocus();
+                calcular = false;
+            }
+            if (b > c + a) {
+                txfladoB.setText("ERRO: b>c+a");
+                txfladoB.requestFocus();
+                calcular = false;
+            }
+            if (c > a + b) {
+                txfladoC.setText("ERRO: c>a+b");
+                txfladoC.requestFocus();
+                calcular = false;
+            }
+
+            // Só procede para o cálculo caso a condição de um lado ser sempre menor ou igual à soma dos outros dois, ser aplicada neste triângulo
+            if (calcular == true) {
+                double somaABC = a + b + c;
+                double ABCdivididoPor2 = somaABC / 2;
+                double area = Math.sqrt((ABCdivididoPor2) * (ABCdivididoPor2 - a) * (ABCdivididoPor2 - b) * (ABCdivididoPor2 - c));
+                System.out.println("\n\nA=" + a + "\nB=" + b + "\nC=" + c + "\n\nÁrea=" + area);
+                txfarea.setText(String.valueOf(df.format(area)));
+                txfarea.setEditable(false);
+            } else {
+                txfarea.setText("Erro!");
+                txfarea.setEditable(false);
+            }
+        }
+
     }
-    
+
+    public static double isNumeric(String str) {
+        try {
+            double num = Double.parseDouble(str);
+            return num;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
 }
-
-
